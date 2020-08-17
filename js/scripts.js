8219e2e4555e73c0549d70b6c9b2c1f5c1b48fa5
diegosalazar343 <salazardiego343@gmail.com>
2020-08-17 13:03:34 -0700

    fix scripts input
    Co-authored-by: Sean Wilson <paintballsean@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#favoriteInput").submit(function (event) {
+  event.preventDefault();
+  const yourFirstName = $("#firstName").val();
+  console.log(yourFirstName);
+  const yourLastName = $("#lastName").val();
+  console.log(yourLastName);
+  const yourFirstThing = $("#firstThing").val();
+  console.log(yourFirstThing);
+  const yourSecondThing = $("#secondThing").val();
+  console.log(yourSecondThing);
+  const yourThirdThing = $("#thirdThing").val();
+  console.log(yourThirdThing);
+
+  const originalArray = [yourFirstName + yourLastName + yourFirstThing + yourSecondThing + yourThirdThing];
+  console.log(originalArray);
+  });
+});
